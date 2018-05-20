69a1a0a15525e39ce0ee0b8bef8939cb7775b0d8
Jacob Jenne <jacob.jenne@careerstep.com>
2018-05-20 17:15:39 -0600

    requirePassword for admin

--- client/src/pages/admin/dashboard/dashboard.js
+++ client/src/pages/admin/dashboard/dashboard.js
@@ -1,33 +1,39 @@
 app.controller("adminDashboardController", function($scope, ReportService) {
 
-  ReportService.getCounts()
-  .then(function(response) {
-    $scope.totalUsers = response.users;
-    $scope.totalLists = response.lists;
-    $scope.totalPlaces = response.places;
-    $scope.$apply();
-  })
-  .catch(function(err) { });
+  requirePassword({
+    afterAuthenticate: function() {
 
-  ReportService.getRecentUsers()
-  .then(function(users) {
-    $scope.recentUsers = users;
-    $scope.$apply();
-  })
-  .catch(function(err) { });
+      ReportService.getCounts()
+      .then(function(response) {
+        $scope.totalUsers = response.users;
+        $scope.totalLists = response.lists;
+        $scope.totalPlaces = response.places;
+        $scope.$apply();
+      })
+      .catch(function(err) { });
 
-  ReportService.getRecentLists()
-  .then(function(lists) {
-    $scope.recentLists = lists;
-    $scope.$apply();
-  })
-  .catch(function(err) { });
+      ReportService.getRecentUsers()
+      .then(function(users) {
+        $scope.recentUsers = users;
+        $scope.$apply();
+      })
+      .catch(function(err) { });
 
-  ReportService.getRecentPlaces()
-  .then(function(places) {
-    $scope.recentPlaces = places;
-    $scope.$apply();
-  })
-  .catch(function(err) { });
+      ReportService.getRecentLists()
+      .then(function(lists) {
+        $scope.recentLists = lists;
+        $scope.$apply();
+      })
+      .catch(function(err) { });
 
+      ReportService.getRecentPlaces()
+      .then(function(places) {
+        $scope.recentPlaces = places;
+        $scope.$apply();
+      })
+      .catch(function(err) { });
+
+    }
+  });
+  
 });
