27cf3e4dd34e3c2dec73e4a29e32c17ca4082561
Jacob Jenne <jacob.jenne@careerstep.com>
2018-06-10 16:40:54 -0600

    now a user can add photos and descriptions from the user-list page

--- server/src/api/place/update-user-place.js
+++ server/src/api/place/update-user-place.js
@@ -1,11 +1,5 @@
 const PlaceModule = require('../../modules/place');
 
 module.exports = function(user, userId, placeId, requestBody) {
-  if (requestBody.isChecked) {
-    return PlaceModule.updateUserPlace(userId, placeId, requestBody);
-  }
-  else {
-    return PlaceModule.deleteUserPlace(userId, placeId)
-    .then(() => '');
-  }
+  return PlaceModule.updateUserPlace(userId, placeId, requestBody);
 }
