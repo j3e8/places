c5b3bdb1fa0c6351c18a0421c7c4a350d52fdf79
Jake Jenne <jake.jenne@nuvi.com>
2019-01-30 10:12:30 -0700

    wip

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -98,3 +98,15 @@ app.config(["$routeProvider", "$locationProvider", function($routeProvider, $loc
     } catch(ex) { }
   });
 });
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.error('ServiceWorker registration failed: ', err);
+    });
+  });
+}
